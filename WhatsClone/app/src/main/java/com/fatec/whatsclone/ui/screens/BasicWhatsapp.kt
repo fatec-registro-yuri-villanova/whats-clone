7f067a20f0bda781fef483b50b83870ab5d8e208
yvillanova <yurimoreiravillanova1@gmail.com>
2025-03-19 21:55:54 -0300

    feat: add animate rotate icon

--- WhatsClone/app/src/main/java/com/fatec/whatsclone/ui/screens/BasicWhatsapp.kt
+++ WhatsClone/app/src/main/java/com/fatec/whatsclone/ui/screens/BasicWhatsapp.kt
@@ -1,6 +1,14 @@
 package com.fatec.whatsclone.ui.screens
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -8,10 +16,24 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Send
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,6 +47,14 @@ import com.fatec.whatsclone.ui.theme.WhatsCloneTheme
 @Composable
 fun WhatsAppScreen() {
     var showInfo by remember { mutableStateOf(false) }
+    // Estado para controlar a rotação
+    val isRotated = remember { mutableStateOf(false) }
+    // Animação da rotação
+    val rotation by animateFloatAsState(
+        targetValue = if (isRotated.value) 45f else 0f,
+        animationSpec = tween(durationMillis = 300),
+        label = "rotationAnimation"
+    )
 
     Scaffold(
         topBar = {
@@ -34,8 +64,17 @@ fun WhatsAppScreen() {
                     IconButton(onClick = { showInfo = true }) {
                         Icon(Icons.Filled.Info, contentDescription = "Info")
                     }
-                    IconButton(onClick = { /* Action for MoreVert icon */ }) {
-                        Icon(Icons.Filled.MoreVert, contentDescription = "More")
+                    IconButton(onClick = {
+                        // Alterna o estado da rotação ao clicar
+                        isRotated.value = !isRotated.value
+                        // Adicione aqui outras lógicas necessárias
+
+                    }) {
+                        Icon(
+                            imageVector = Icons.Filled.MoreVert,
+                            contentDescription = "Mais opções",
+                            modifier = Modifier.rotate(rotation) // Aplica a rotação animada
+                        )
                     }
                 }
             )
